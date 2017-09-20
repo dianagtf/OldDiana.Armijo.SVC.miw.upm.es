8bb46ca0e34411e426da09553d4a82c69d13268f
Diana <Diana@DianaPC>
2017-09-20 22:54:07 +0200

    Create Class C12Test

---
+++ src/test/java/miw/upm/es/Diana/Armijo/SVC/C12Test.java
@@ -0,0 +1,21 @@
+package miw.upm.es.Diana.Armijo.SVC;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C12Test {
+
+	private C12 c12;
+
+    @Before
+    public void before() {
+        c12 = new C12();
+    }
+    
+    @Test
+    public void testMA() {
+        assertEquals("", c12.mA());
+    }
+}
