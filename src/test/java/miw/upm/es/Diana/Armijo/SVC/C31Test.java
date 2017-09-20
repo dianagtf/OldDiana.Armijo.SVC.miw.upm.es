b211d5b6cf3b9da7b43dfdf50c142854152012bf
Diana <Diana@DianaPC>
2017-09-20 22:52:32 +0200

    Create Class C31Test

---
+++ src/test/java/miw/upm/es/Diana/Armijo/SVC/C31Test.java
@@ -0,0 +1,25 @@
+package miw.upm.es.Diana.Armijo.SVC;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C31Test {
+	private C31 c31;
+	
+    @Before
+    public void before() {
+        c31 = new C31();
+    }
+    
+    @Test
+    public void testM1() {
+        assertEquals("", c31.m1());
+    }
+    
+    @Test
+    public void testM2() {
+        assertEquals("", c31.m2());
+    }
+}
